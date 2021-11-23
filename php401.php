20d448ed99dcec33efe6e94fb9b1c14b21b9613d
kawinnat09 <kwinnathmyura@gmail.com>
2021-11-23 17:32:37 +0700

    update

--- php401.php
+++ php401.php
@@ -1,4 +1,36 @@
 <?php
 
-echo "Hello Week4";
+//calling by function
+function welcome(){
+    echo "Hi, welcome to Thailand";
+}
+
+//calling by value
+function sayHi($name,$age){
+    //echo "Hello Week4 <br />";
+    echo "$name $age";
+}
+
+//calling by Reference
+function adder(&$str,&$std1){
+    $str = "Call By Reference";
+    $std1 = "Jame";
+}
+
+//Default argument Value
+function lastName($name="Sakaberg"){
+    echo "Hello $name<br />";
+}
+
+welcome(); //calling by function
+echo "<br />";
+sayHi("Mark",25); //calling by value
+echo "<br />";
+sayHi("Jhon",30); //calling by value
+echo "<br />";
+adder($str,$std1); //calling by Reference
+echo $str," ",$std1;;
+echo "<br />";
+echo lastName();
+
 ?>
---
+++ php404.php
@@ -0,0 +1,10 @@
+<?php
+
+function inc(&$i){
+    echo ++$i;
+}
+
+$i = 10;
+inc($i);
+
+?>
